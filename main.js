ea4be83aff1fdded585a596d4b50bb5f14f8d948
Eric London <github@ericlondon.com>
2022-10-05 15:59:29 -0400

    comment

--- main.js
+++ main.js
@@ -14,4 +14,6 @@ const html = fs.readFileSync("index.html").toString();
 const dom = new JSDOM(html, { runScripts: "dangerously" });
 
 const result = dom.window.eval("getSomething()");
+
+// output: Some text here
 console.log(result)
